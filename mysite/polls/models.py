d06063685baf45d85a5b38528d7b30ebf7130594
Cabilangan112 <jassencabilangan@gmail.com>
2018-04-16 10:12:03 +0800

    first

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,29 @@
-from django.db import models
+# -*- coding: utf-8 -*-
 
-# Create your models here.
+from __future__ import unicode_literals
+
+from django.db import models
+from Faculty.models import detail
+
+class Question(models.Model):
+
+	question_text = models.CharField(max_length=500)
+	pub_date = models.DateTimeField('date published')
+	
+
+	def __str__(self):
+		return self.question_text
+		
+	def was_published_recently(self):
+		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+		
+		
+class Choice(models.Model):
+	Name_of_Faculty = models.ForeignKey(detail, on_delete=models.CASCADE)
+	question = models.ForeignKey(Question, on_delete=models.CASCADE)
+	choice_text = models.CharField(max_length=200)
+	votes = models.IntegerField(default=0)
+	comment = models.TextField()
+	
+	def __str__(self):
+		return self.choice_text
